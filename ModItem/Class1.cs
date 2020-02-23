9e62b57c3d8b5790b9e5f86caaadb92b398e9a71
Rasmus Hertzberg <rasmus.hertzbergrgmail.com>
2020-02-23 17:33:21 +0100

    Added code

---
+++ ModItem/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using BepInEx;
+using RoR2;
+
+
+namespace ModItem
+{
+    [BepInDependency("com.bepis.r2api")]
+    [BepInPlugin("com.TeelTigh.Hurtitem", "Thorns Potion", "0.0.1")]
+    public class Class1 : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            ItemDef itemDef = new ItemDef
+            {
+                name = "Thorns Potion",
+                canRemove = true,
+                pickupIconPath = "Thorns Potion",
+                tier = ItemTier.Tier2
+                
+            };
+            ItemTag[] tagArray = new ItemTag[1];
+            ItemDef itemDef2 = itemDef;
+            itemDef2.tags = tagArray;
+            itemDef.unlockableName = "sss";
+
+        }
+    }
+}
